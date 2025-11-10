7d72cc19074bd385405f28e220645f31633cb511
PavanKumarBollu <bollupavankumar12@gmail.com>
2025-11-10 11:56:52 +0530

    Notations, SchemaCreation, TablesCRUD Operations

---
+++ SQL_Examples_Queries.sql
@@ -0,0 +1,18 @@
+-- Retriving the data
+
+-- SELECT ID, Name, CountryCode from world.city 
+
+-- SELECT ID,Name,CountryCode, Population from world.city WHERE Population > 5000000
+
+
+ -- SELECT ID,Name,CountryCode, Population from world.city WHERE Population > 5000000 order by Population
+ 
+  -- SELECT min(Population) from world.city 
+  
+  -- insert into world.city (id, Name, CountryCode, District, Population) values (10000, "INDIA","IND","HYD",1000000)
+  
+  -- DELETE FROM world.city where id = 1000
+  
+  -- update world.city set countrycode = "IND" where id = 1
+  -- update world.city set population = 0 where id = 1
+  
