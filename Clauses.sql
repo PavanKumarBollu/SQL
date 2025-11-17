78d269d038c4307a939669d3dfc52ce461266ec5
PavanKumarBollu <bollupavankumar12@gmail.com>
2025-11-17 11:48:48 +0530

    BoleeanAndAggregative functions

--- Clauses.sql
+++ Clauses.sql
@@ -35,10 +35,61 @@ select * from address where city like "s%";
 --  select * from disease where name like "%e_"; 
 
 
+-- boolean operators
+-- AND OR BETWEEN 
+-- Agrregate functions
+
+
+-- AND 
+select * from patientinsurance ;
+select * from patientinsurance where providername = "HealthCo" ; 
+select * from patientinsurance where providername = "HealthCo"  AND outnetworkcopay = 30;
+select * from patientinsurance where providername = "HealthCo"  AND outnetworkcopay = 40;
 
 
 
+-- OR 
+select * from patientinsurance ;
+select * from patientinsurance where providername = "HealthCo" or providername = "CarePlus" ;
 
 
+-- the following query will give us some unwanted records to avoid those use the next following query
+-- select * from patientinsurance where providername = "HealthCo" or providername = "CarePlus" and outnetworkcopay = 40;
+
+
+select * from patientinsurance where (providername = "HealthCo" or providername = "CarePlus") and outnetworkcopay = 40 ;
+select * from patientinsurance where outnetworkcopay = 40 and (providername = "HealthCo" or providername = "CarePlus");
+
+
+-- between 
+select * from patientinsurance;
+select * from patientinsurance where PatientId between 3 and 9;
+
+
+-- AGGREGATIVE FUNCTIONS 
+-- count min max avg sum 
+
+-- Count
+select count(PatientId) from patientinsurance ; 
+select count(ModifiedOn) from patientinsurance ; 
+
+select count(*) from patientinsurance where outnetworkcopay = 40 and (providername = "HealthCo" or providername = "CarePlus");
+select count(*) from patientinsurance where providername = "HealthCo" or providername = "CarePlus" and outnetworkcopay = 40;
+
+-- SUM 
+select sum(InNetworkCoPay) from patientInsurance;
+select sum(InNetworkCoPay) from patientInsurance where patientId = 5 or patientId = 7;
+
+-- AVG 
+select avg(InNetworkCoPay) from patientInsurance;
+select avg(InNetworkCoPay) from patientInsurance where patientId = 5 or patientId = 7;
+
+-- Min 
+select min(InNetworkCoPay) from patientInsurance;
+select min(InNetworkCoPay) from patientInsurance where patientId = 3 or patientId = 11;
+
+-- max
+select max(InNetworkCoPay) from patientInsurance;
+select max(InNetworkCoPay) from patientInsurance where patientId = 5 or patientId = 7;
 
 
